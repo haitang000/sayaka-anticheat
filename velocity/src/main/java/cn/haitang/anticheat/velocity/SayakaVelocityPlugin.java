61b6c831d2d46fcbdd88aa71da49f4131b9eeb7e
haitang000 <3603866430@qq.com>
2026-08-04 04:34:52 -0400

    chore: bump version to 2.2.0.1-beta.2

--- velocity/src/main/java/cn/haitang/anticheat/velocity/SayakaVelocityPlugin.java
+++ velocity/src/main/java/cn/haitang/anticheat/velocity/SayakaVelocityPlugin.java
@@ -42,7 +42,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @Plugin(
         id = "sayaka-anticheat",
         name = "Sayaka AntiCheat Velocity",
-        version = "2.2.0.1-beta.1",
+        version = "2.2.0.1-beta.2",
         authors = {"haitang"}
 )
 public final class SayakaVelocityPlugin {
